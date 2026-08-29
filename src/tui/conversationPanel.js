1ef009e3c8cb6c861983a5d610c15b886b5fadd1
Jason Mulligan <jason.mulligan@avoidwork.com>
2026-08-29 18:10:09 -0400

    fix: remove unused memo import from conversationPanel.js

--- src/tui/conversationPanel.js
+++ src/tui/conversationPanel.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, memo } from "react";
+import React, { useRef, useEffect } from "react";
 import { Box } from "ink";
 import { MessageList } from "./messageList.js";
 
