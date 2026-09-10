d4f2a00278bf6963a73d718c0363360f29ef325d
Jason Mulligan <jason.mulligan@avoidwork.com>
2026-09-10 06:44:11 -0400

    fix: default fulltext to true so hybrid mode has FTS data

--- src/tools/codeIndex/index.js
+++ src/tools/codeIndex/index.js
@@ -37,7 +37,7 @@ export async function codeIndexImpl(input) {
 
 		try {
 			const store = await createVectorStore(proj.dbPath, {
-				fulltext: proj.fulltext || false,
+				fulltext: proj.fulltext !== false,
 				ftsTokenize: proj.ftsTokenize || "porter unicode61",
 			});
 			await store.init();
--- src/tools/codeSearch/index.js
+++ src/tools/codeSearch/index.js
@@ -37,7 +37,7 @@ export async function codeSearchImpl(input, options = {}) {
 	let store;
 	try {
 		store = await createVectorStore(dbPath, {
-			fulltext: proj.fulltext || false,
+			fulltext: proj.fulltext !== false,
 			ftsTokenize: proj.ftsTokenize || "porter unicode61",
 		});
 		store.init();
