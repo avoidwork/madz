52c1ad5a06a3fbd711d5a9c7711bac62a931185c
Jason Mulligan <jason.mulligan@avoidwork.com>
2026-09-11 11:19:47 -0400

    fix: add missing config import in memory tool
    The memory tool wrapper referenced config.memory.maxEntries and
    config.memory.contextDir without config being defined in scope.
    loadConfig() was called on line 7 but only cwd was extracted.
    
    This caused a ReferenceError: config is not defined when the memory
    tool was invoked.
    
    Fixes #1031

--- src/tools/memory/index.js
+++ src/tools/memory/index.js
@@ -4,7 +4,8 @@ import { mkdir, writeFile, readFile, readdir, unlink, access } from "node:fs/pro
 import { join, basename } from "node:path";
 import { loadConfig } from "../../config/loader.js";
 
-const cwd = loadConfig().cwd;
+const config = loadConfig();
+const cwd = config.cwd;
 
 const DEFAULT_MAX_ENTRIES = 100;
 
